87235ea42960f9feecc5c84a264703fbfb4f7661
Debora Lara <debora.roldanlara@gmail.com>
2020-04-11 21:09:17 -0500

    aplicações JS

--- script.js
+++ script.js
@@ -21,3 +21,32 @@ console.log(h2Animais);
 const paragrafos = document.querySelectorAll('p');
 
 console.log(paragrafos[--paragrafos.length]);
+
+// Mostre no console cada parágrado do site
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos);
+
+paragrafos.forEach((item) => {
+  console.log(item);
+});
+
+// Mostre o texto dos parágrafos no console
+paragrafos.forEach((item) => {
+  console.log(item.innerText);
+});
+
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => i++);
+
+console.log(i);
